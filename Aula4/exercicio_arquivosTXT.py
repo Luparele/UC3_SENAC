6befa18789463fe89c41a05312bb70208868dd17
Eduardo Luparele coelho <150554762+Luparele@users.noreply.github.com>
2025-05-06 07:39:33 -0300

    somente proposta do exercício

--- Aula4/exercicio_arquivosTXT.py
+++ Aula4/exercicio_arquivosTXT.py
@@ -3,4 +3,8 @@
 Armazene usuários (nome, e-mail) em um arquivo.
 Liste todos os usuários.
 Permita excluir um usuário.
+------------------------------------------------------
+
+Exercício realizado e salvo na pasta "UC3_SENAC/Exercício aula 4"
+
 '''
